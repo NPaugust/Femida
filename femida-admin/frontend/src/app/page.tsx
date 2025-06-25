5cc3d92162bc3d462d9b8f3288c3840928026b94
NPaugust <avgust1ne.k6@gmail.com>
2025-06-25 23:10:53 +0600

    Fix

--- femida-admin/frontend/src/app/page.tsx
+++ femida-admin/frontend/src/app/page.tsx
@@ -171,7 +171,7 @@ export default function Dashboard() {
                 </div>
               ))}
             </div>
-          </div>
+        </div>
           
           {renderRoomHouses()}
         </>
