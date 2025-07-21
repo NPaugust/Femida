07be9e641183485da5e725d22e28335c76888251
NPaugust <avgust1ne.k6@gmail.com>
2025-07-21 17:30:52 +0600

    Femida

--- frontend/src/components/AppProviders.tsx
+++ frontend/src/components/AppProviders.tsx
@@ -3,16 +3,17 @@ import { Provider } from 'react-redux';
 import { store, setAuth } from '../app/store';
 import I18nProvider from './I18nProvider';
 import { useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 export default function AppProviders({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    // Инициализация auth из localStorage
-    const access = localStorage.getItem('access');
-    const refresh = localStorage.getItem('refresh');
-    const role = localStorage.getItem('role');
-    let user = null;
+    // Инициализация auth из cookies
+    const access = Cookies.get('access');
+    const refresh = Cookies.get('refresh');
+    const role = Cookies.get('role');
+    let user = undefined;
     try {
-      const userStr = localStorage.getItem('user');
+      const userStr = Cookies.get('user');
       if (userStr) user = JSON.parse(userStr);
     } catch {}
     if (access || refresh || role || user) {
